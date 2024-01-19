81dfcaae051155a06bb966b521ffb174f355ee15
gustavorods <gustavorodrigues@pop-os.localdomain>
2024-01-19 16:13:41 -0300

    feat css: implementação de css no projeto

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-// Elementos 
-const input = document.querySelector('input'); 
+const input = document.querySelector('input');
 const lista = document.querySelector('ul');
 
+function handKeyUp(event) { //O "event" pega os detalhes do evento 
+    const textoDoInput = input.value;
 
-//funções
-function handKeyUp(event) //chamando os detalhes do evento 
-{   
-    const textoDoInput = input.value; //esse valor é igual ao que foi digitado no input (para chamar o valor que foi digitado no input, é so colcoar o .value)
-
-
-    if (textoDoInput.trim() === "") 
-    {
-        alert("Você precisa digital algo!")
-    }
-
-
-    else if(event.key === "Enter") // se a tecla apertada for igual a "enter"
-    {
-        const newli = document.createElement('li'); //criando o newli
-        newli.innerHTML = textoDoInput; // colocando um valor nesse newli, 
-        lista.appendChild(newli); //adiconando esse novo li
-
-        textoDoInput = ""; //resetando o input
+    if(event.key === "Enter" && textoDoInput.trim() !== "") {
+        const newli = document.createElement('li');
+        newli.innerHTML = textoDoInput;
+        lista.appendChild(newli);
+        input.value = "";
     }
 }
 
-
-// Eventos 
 input.addEventListener("keyup", handKeyUp);
-
